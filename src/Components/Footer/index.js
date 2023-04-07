889b6b10223facf109dda74d54ef56d52694f166
daniel molina <bigdpower@icloud.com>
2023-04-07 10:44:25 -0500

    added contact info

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -8,6 +8,21 @@ const Footer = () => {
   return (
     <Container fluid className="footer">
       <Row>
+        <h4 className="white">For more information please contact</h4>
+      <Col className="white">
+        <p>Mireya Perez-Power</p>
+        <p>President & Director</p>
+        <p>Email: Ayerimzerep@gmail.com</p>
+        <p>Phone: 305-443-0770</p>
+      </Col>
+      <Col className="white">
+      <p>Martha Power-Perez</p>
+        <p>Coordinator</p>
+        <p>Email: marthapower@mac.com</p>
+        <p>Phone: 786-712-2872</p>
+      </Col>
+      </Row>
+      <Row>
         <Col md="true" className="footer-copywright">
           <h3>Copyright © {year} PIAG MUSEUM</h3>
         </Col>
